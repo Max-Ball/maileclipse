2793ea0c8621cdf314cd4e8c099bedda3dd5f60f
Max Ball <max.ball3388@gmail.com>
2022-12-12 21:59:55 -0700

    add template

--- client/src/views/AddTemplate.vue
+++ client/src/views/AddTemplate.vue
@@ -14,8 +14,8 @@
         </div>
         <div
           class="w-full max-w-6xl space-y-8 bg-white my-4 shadow-md rounded animate-fade-in-down relative block appearance-none">
-          <div
-            class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+          <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400
+            dark:border-gray-700">
             <ul class="flex flex-wrap -mb-px">
               <li class="mr-2">
                 <a href="#html"
